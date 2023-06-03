5580dec0d584b305974c944b0f0b16d8608bb60f
amanph <122424467+amanph@users.noreply.github.com>
2023-06-03 13:17:39 +0300

    changed img source

--- componets/Navbar.js
+++ componets/Navbar.js
@@ -2,7 +2,11 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <img src="./../img/r.png" alt="react-logo" className="nav--icon" />
+      <img
+        src="https://github.com/amanph/stackblitz-starters-afzmuz/blob/main/img/r.png"
+        alt="react-logo"
+        className="nav--icon"
+      />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
